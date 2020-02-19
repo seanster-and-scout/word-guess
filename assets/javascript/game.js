7e6285e85ce0e0e2d467f55266fc326b505ed6cf
Sean Baklini <sbaklini@unm.edu>
2020-02-18 21:50:53 -0700

    Got random letters working correcting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,41 @@
 $(document).ready(function() {
-  const dictionary = ["Hey Jude", "Come Together", "Sargent Pepper"];
-  let randomNumber = Math.floor(Math.random() * dictionary.length);
-  let video1 = $("iframe").attr(
-    "src",
-    "https://www.youtube.com/watch?v=7qMls5yxP1w"
-  );
-  let video2 = $("iframe").attr(
-    "src",
-    "https://www.youtube.com/watch?v=oolpPmuK2I8"
-  );
-  let video3 = $("iframe").attr(
-    "src",
-    "https://www.youtube.com/watch?v=naoknj1ebqI"
-  );
-  const videos = [video1, video2, video3];
-  let currentWord = dictionary[randomNumber];
-  let underscore = [];
-  let currentWordArray = currentWord.split("");
-  let guesses = 7;
+  const dictionary = ["HeyJude", "ComeTogether", "SargentPepper"];
+  let randomNumber;
+  //   let video1 = $("iframe").attr(
+  //     "src",
+  //     "https://www.youtube.com/watch?v=7qMls5yxP1w"
+  //   );
+  //   let video2 = $("iframe").attr(
+  //     "src",
+  //     "https://www.youtube.com/watch?v=oolpPmuK2I8"
+  //   );
+  //   let video3 = $("iframe").attr(
+  //     "src",
+  //     "https://www.youtube.com/watch?v=naoknj1ebqI"
+  //   );
+  //   const videos = [video1, video2, video3];
+  let currentWord;
+  let underscore;
+  let currentWordArray;
+  let guesses;
   let guessCheck = false;
-  let lettersGuessed = "";
+  let lettersGuessed;
 
   newGame();
 
   function getRandomWord() {
-    underscore = [];
     randomNumber = Math.floor(Math.random() * dictionary.length);
-    currentWord = dictionary[randomNumber];
-    return currentWord;
+    underscore = [];
+    currentWord = dictionary[randomNumber].toUpperCase();
+    currentWordArray = currentWord.split("");
+    initializeArray(currentWord);
   }
 
-  function getRandomSong() {
-    return videos[randomNumber];
-  }
+  //   function getRandomSong() {
+  //     return videos[randomNumber];
+  //   }
 
-  function initializeArray() {
+  function initializeArray(currentWord) {
     for (let i = 0; i < currentWord.length; i++) {
       underscore.push("_ ");
     }
@@ -59,29 +60,34 @@ $(document).ready(function() {
 
   function newGame() {
     getRandomWord();
-    getRandomSong();
-    initializeArray();
+    //getRandomSong();
     setWord();
     guesses = 7;
     guessCheck = false;
+    lettersGuessed = "";
     $("#Guesses").html("Guesses Remaining: " + guesses);
   }
 
   $(document).keydown(function(event) {
     let code = event.keyCode;
     let letter = String.fromCharCode(code);
-    for (let i = 0; i < currentWord.length; i++) {
-      if (currentWordArray[i].toUpperCase()) {
-        underscore[i] = currentWordArray[i];
-        $("#underscore").html(underscore);
-        guessCheck = true;
+    if (code >= 48 && code <= 90) {
+      for (let i = 0; i < currentWord.length; i++) {
+        if (currentWordArray[i] == letter) {
+          underscore[i] = currentWordArray[i];
+          $("#underscore").html(underscore);
+          guessCheck = true;
+        }
+        if (
+          i == currentWord.length - 1 &&
+          lettersGuessed.indexOf(letter) == -1
+        ) {
+          lettersGuessed += letter;
+        }
       }
-      if (i == currentWord.length - 1) {
-        lettersGuessed += letter;
+      if (guessCheck == false && guesses > 0) {
+        guesses--;
       }
-    }
-    if (guessCheck == false && guesses > 0) {
-      guesses--;
     }
 
     getLettersGuessed();
